44e2f02acb4e42917089634732fcce4841bcc2d4
whyvas <whyvas@gmail.com>
2016-03-14 00:39:06 +0000

    blah

---
+++ arduino/src/sketch.ino
@@ -0,0 +1,18 @@
+void setup() {  
+    Serial.begin(115200);
+    delay(2000);
+    while (!Serial.available()) {
+      Serial.write(0x01);
+      delay(3000);
+}
+    // read the byte that Python will send over
+    Serial.read();
+
+}
+void loop(){
+int fart=0;
+while(1){
+Serial.print(fart++);
+delay(10);
+}
+}
